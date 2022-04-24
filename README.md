59e40a0b5f3489cc7804d26704a3f7ced90c9b0e
kamalasaurus <kamalasaurus@gmail.com>
2022-04-24 13:58:17 -0400

    working sample

---
+++ README.md
@@ -0,0 +1,15 @@
+# simpletest
+
+A super simple test-runner for in-browser personal projects.  Primarily
+for my own usage so I don't have to keep shunting this file around.
+
+### usage
+
+1. Import from jsDelivr, pull down from github/npm (whatever floats your
+   boat) into html with javascript you want to test.
+2. Look at test.html for an example of how to run it
+3. Run a server in the root of your directory (i.e. `static-server`)
+4. Navigate to the html page in your browser (i.e.
+   `localhost:9080/test.html`)
+5. Open the developer console, and behold your tests in all their glory
+   ✨
---
+++ simpletest.js
@@ -0,0 +1,19 @@
+export default function simpletest(suite, clearDOM = true) {
+  suite.forEach(([slug, statement, assertion], i) => {
+    try {
+      const s = statement()
+      const a = assertion(s)
+
+      // it's easy to drop a debugger in based on iteration number
+      // either here or in the assertion or the statement bodies
+      //if (i===1) debugger
+
+      if (!a) console.error(`${String.fromCodePoint(0x274c)} test ${i} FAILED ${slug}`)
+      if (a) console.log(`%c${String.fromCodePoint(0x2705)} test ${i} SUCCESS ${slug}`, 'color: lightgreen;')
+    } catch(e) {
+      console.error(`${String.fromCodePoint(0x274c)} test ${i} ERROR ${slug} ${e}`, 'color: red')
+    } finally {
+      if(clearDOM) document.body.innerHTML = ''
+    }
+  })
+}
